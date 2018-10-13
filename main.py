c8c1abe1bc6877624dd66bf8d4b107f4c7a36aaa
MrWormHole <mrwormhole@github.com>
2018-10-13 23:55:05 +0300

    :beetle: bug fixes and more features and platforms

--- main.py
+++ main.py
@@ -17,8 +17,15 @@ class Game:
 
     def new(self):
         self.all_sprites = pg.sprite.Group()
-        self.player = Player(GREEN)
+        self.all_platforms = pg.sprite.Group()
+        self.player = Player(30,GREEN) #experimenting giving game class to player
         self.all_sprites.add(self.player)
+        
+        for platform in PLATFORM_LIST:
+            p = Platform(platform[0],platform[1],platform[2],platform[3], platform[4])
+            self.all_sprites.add(p)
+            self.all_platforms.add(p)
+
         self.run()
 
     def run(self):
@@ -36,9 +43,19 @@ class Game:
                 if self.playing == True:
                     self.playing = False
                 self.running = False
+            if event.type == pg.KEYDOWN:
+                hits = pg.sprite.spritecollide(self.player,self.all_platforms,False)
+                if event.key == pg.K_SPACE and hits:
+                    self.player.jump()
 
     def update(self):
         self.all_sprites.update()
+        #check if player hits a Platform - only if falling
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.all_platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top + 1
+                self.player.vel.y = 0
 
     def draw(self):
         self.screen.fill(BLUE)
--- sprites.py
+++ sprites.py
@@ -3,9 +3,10 @@ from settings import *
 vec = pg.math.Vector2 #for additional movement
 
 class Player(pg.sprite.Sprite):
-    def __init__(self,color):
+    def __init__(self,size,color):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((30,30))
+        self.size = size
+        self.image = pg.Surface((self.size,self.size))
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH /2 , HEIGHT /2)
@@ -15,10 +16,13 @@ class Player(pg.sprite.Sprite):
         self.vel = vec(0,0) #for additional movement
         self.acc = vec(0,0) #for additional movement
 
+    def jump(self):
+        self.vel.y = PLAYER_JUMP_SPEED
+
     def update(self):
         #self.velocityX = 0 #for additional movement
         #self.velocityY = 0 #for additional movement
-        self.acc = vec(0,0) #for additional movement
+        self.acc = vec(0,PLAYER_GRAVITY) #for additional movement
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
             #self.velocityX = -5 #for additional movement
@@ -35,14 +39,22 @@ class Player(pg.sprite.Sprite):
 
         #self.rect.x += self.velocityX #for additional movement
         #self.rect.y += self.velocityY #for additional movement
-        self.acc += self.vel * PLAYER_FRICTION
+        self.acc.x += self.vel.x * PLAYER_FRICTION
         self.vel += self.acc #for additional movement. Notice that it is missing time from physics
         self.pos += self.vel + 0.5 * self.acc #for additional movement Notice that it is missing time from physics
-		
-        #little bit weird i dont know how i feel about this
-        if self.pos.x - 15 > WIDTH:
-            self.pos.x = 0
-        if self.pos.x + 15 < 0:
-            self.pos.x = WIDTH
 
-        self.rect.center = self.pos
+        if self.pos.x - self.size/2 > WIDTH:
+            self.pos.x = 0 - self.size/2
+        if self.pos.x + self.size < 0:
+            self.pos.x = WIDTH + self.size/2
+
+        self.rect.midbottom = self.pos
+
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y, width, height, color):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((width,height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
